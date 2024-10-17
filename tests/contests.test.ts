19d47eef42b6ad4777942401e3a45af173ed0fcf
Alien Deployer <m@a17.name>
2024-10-18 02:24:11 +0300

    cover contest types by tests

--- tests/contests.test.ts
+++ tests/contests.test.ts
@@ -1,9 +1,13 @@
-import {contests} from "../src";
+import {contests, RewardType, YieldContest} from "../src";
 
 describe('testing contests', () => {
   test('check start less then end', () => {
     for (const contestId of Object.keys(contests)) {
       expect(contests[contestId].start).toBeLessThan(contests[contestId].end)
     }
+  })
+  test('types', () => {
+    const c: YieldContest = contests["y1"]
+    expect(c.rewards[0].type).toEqual(RewardType.POINTS)
   })
 })
