f95dfdcf62bd441d125a408677acdd3d047b5349
RenzouM <renzoum@gmail.com>
2024-11-09 05:47:35 -0300

    🪙 Add blue chip assets for Optimism [10]

--- src/assets.ts
+++ src/assets.ts
@@ -9,6 +9,8 @@ export type Asset = {
 export const assets: Asset[] = [
   {
     addresses: {
+      "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      "10": "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
       "137": [
         "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
         "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
@@ -18,7 +20,6 @@ export const assets: Asset[] = [
         "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
       ],
       "111188": "0xc518A88c67CECA8B3f24c4562CB71deeB2AF86B7",
-      "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
       "42161": [
         "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
         "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
@@ -33,9 +34,10 @@ export const assets: Asset[] = [
   },
   {
     addresses: {
+      "1": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      "10": "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
       "137": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
       "8453": "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
-      "1": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
       "42161": "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
       "42420": "0x26E490d30e73c36800788DC6d6315946C4BbEa24",
     },
@@ -47,11 +49,12 @@ export const assets: Asset[] = [
   },
   {
     addresses: {
+      "1": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      "10": "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
       "137": "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
       "8453": "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
-      "111188": "0x75d0cBF342060b14c2fC756fd6E717dFeb5B1B70",
-      "1": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
       "42161": "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+      "111188": "0x75d0cBF342060b14c2fC756fd6E717dFeb5B1B70",
     },
     symbol: "DAI",
     description:
@@ -71,9 +74,10 @@ export const assets: Asset[] = [
   },
   {
     addresses: {
+      "1": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      "10": "0x4200000000000000000000000000000000000006",
       "137": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
       "8453": "0x4200000000000000000000000000000000000006",
-      "1": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       "42161": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
       "42420": "0xbe231A8492487aAe6096278A97050FAe6B9d5BEc",
     },
@@ -84,8 +88,9 @@ export const assets: Asset[] = [
   },
   {
     addresses: {
-      "137": "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
       "1": "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+      "10": "0x68f180fcCe6836688e9084f035309E29Bf0A2095",
+      "137": "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
       "42161": "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
       "42420": "0xDBDc8c7B96286899aB624F6a59dd0250DD4Ce9bC",
     },
@@ -211,6 +216,16 @@ export const assets: Asset[] = [
     symbol: "SACRA",
     description: "Dark Fantasy on-chain RPG",
     website: "https://sacra.game",
+    color: "#ff0000",
+  },
+  {
+    addresses: {
+      "10": "0x4200000000000000000000000000000000000042",
+    },
+    symbol: "OP",
+    description:
+      "OP is the native token of Optimism, a Layer 2 scaling solution for Ethereum. It is used for governance, incentives, and security through staking.",
+    website: "https://www.optimism.io/",
     color: "#ff0000",
   },
 ];
