dcb4131d4579e642b8b157f56b5339abe0722b4c
Alien Deployer <m@a17.name>
2025-02-13 03:05:33 +0300

    getContestReward, getContestGemsReward; y10-y17 dont have points

--- tests/contests.test.ts
+++ tests/contests.test.ts
@@ -1,4 +1,4 @@
-import { contests, Reward, RewardType, YieldContest } from "../src";
+import { contests, getContestGemsReward, getContestReward, Reward, RewardType, YieldContest } from "../src";
 
 describe("testing contests", () => {
   test("check start less then end", () => {
@@ -9,5 +9,23 @@ describe("testing contests", () => {
   test("types", () => {
     const c: YieldContest = contests["y1"];
     expect((c.rewards[0] as Reward).type).toEqual(RewardType.POINTS);
+  });
+  test("getContestReward", () => {
+    const c: YieldContest = contests["y9"];
+    const rG = getContestReward(c, RewardType.GEMS1)
+    expect(rG?.totalReward).toEqual(900000);
+    const rP = getContestReward(c, RewardType.POINTS)
+    expect(rP?.winners).toBeGreaterThan(49);
+    const c30: YieldContest = contests["y30"];
+    const und = getContestReward(c30, RewardType.POINTS)
+    expect(und).toEqual(undefined)
+  });
+  test("getContestGemsReward", () => {
+    const c: YieldContest = contests["y9"];
+    const rG = getContestGemsReward(c)
+    expect(rG?.totalReward).toEqual(900000);
+    const c1 = contests["y1"]
+    const und = getContestGemsReward(c1)
+    expect(und).toEqual(undefined)
   });
 });
