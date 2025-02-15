b2621a4f39abb7df47911b1f8657861dd4ff3da3
Alien Deployer <m@a17.name>
2025-02-16 00:41:54 +0300

    🪙 sonicWhitelistedAssets

--- src/assets.ts
+++ src/assets.ts
@@ -585,6 +585,25 @@ export const assets: Asset[] = [
   },
 ];
 
+export const sonicWhitelistedAssets: {
+  [addrLc: `0x${string}`]: number;
+} = {
+  // scUSD, stkscUSD: 6x (Boosted)
+  ["0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE".toLowerCase()]: 6,
+  // USDC.e: 5x (Boosted)
+  ["0x29219dd400f2Bf60E5a23d13Be72B486D4038894".toLowerCase()]: 5,
+  // scETH, stkscETH: 4x (Boosted)
+  ["0x3bcE5CB273F0F148010BbEa2470e7b5df84C7812".toLowerCase()]: 4,
+  // s, wS, stS, OS, wOS: 4x (Boosted)
+  ["0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38".toLowerCase()]: 4,
+  ["0xE5DA20F15420aD15DE0fa650600aFc998bbE3955".toLowerCase()]: 4,
+  ["0xb1e25689D55734FD3ffFc939c4C3Eb52DFf8A794".toLowerCase()]: 4,
+  ["0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38".toLowerCase()]: 4,
+  // ONE, WETH: 2x (Boosted)
+  ["0x50c42dEAcD8Fc9773493ED674b675bE577f2634b".toLowerCase()]: 2,
+  // SolvBTC and SolvBTC.BBN: 2x (Boosted)
+};
+
 export const getAsset = (
   chainId: string,
   tokenAddress: `0x${string}`,
--- tests/assets.test.ts
+++ tests/assets.test.ts
@@ -1,4 +1,4 @@
-import { getAsset, assets } from "../src";
+import { getAsset, assets, sonicWhitelistedAssets } from "../src";
 import { tokenlist } from "../src";
 import { getTokenData } from "../src";
 
@@ -64,5 +64,12 @@ describe("testing assets", () => {
     expect(tokenData?.symbol).toEqual("WETH");
     tokenData = getTokenData("0x00eb23fd6bc0add59e62ac25578270cff1b9f619");
     expect(tokenData).toEqual(undefined);
+  });
+  test("sonicWhitelistedAssets", () => {
+    const passivePoints =
+      sonicWhitelistedAssets[
+        "0x50c42dEAcD8Fc9773493ED674b675bE577f2634b".toLowerCase() as `0x${string}`
+      ];
+    expect(passivePoints).toEqual(2);
   });
 });
