adf2d67276981e88bd3bdcfc4192677229c9e8f0
mttn3773 <mttn3773@gmail.com>
2025-08-26 23:19:09 +0300

    add metavaults type

--- src/deployments.ts
+++ src/deployments.ts
@@ -34,6 +34,7 @@ export type Deployment = {
     symbol: string;
     wrapper: `0x${string}`;
     primary?: boolean;
+    type?: string;
   }[];
 };
 
@@ -103,28 +104,33 @@ export const deployments: { [chainId: string]: Deployment } = {
       {
         address: "0x1111111199558661Bf7Ff27b4F1623dC6b91Aa3e",
         symbol: "metaUSD",
+        type: "MetaVault",
         wrapper: "0xAaAaaAAac311D0572Bffb4772fe985A750E88805",
         primary: true,
       },
       {
         address: "0x4444444420D9De54d69b3997b7D6A31d2BF63F32",
         symbol: "metaS",
+        type: "MetaVault",
         wrapper: "0xbbbbbbBBbd0aE69510cE374A86749f8276647B19",
         primary: true,
       },
       {
         address: "0x22222222780038f8817b3dE825a070225e6d9874",
         symbol: "metaUSDC",
+        type: "MultiVault",
         wrapper: "0xEEEEEEE6d95E55A468D32FeB5d6648754d10A967",
       },
       {
         address: "0x33333333C480194b5B651987b7D00B20dDCbd287",
         symbol: "metascUSD",
+        type: "MultiVault",
         wrapper: "0xccccCCcca9FC69a2b32408730011EdB3205A93A1",
       },
       {
         address: "0x555555554776B14B30597d1032E48f9e16db22A4",
         symbol: "metawS",
+        type: "MultiVault",
         wrapper: "0xffFFFFFf2fcBeFAe12F1372C56edC769BD411685",
       },
     ],
